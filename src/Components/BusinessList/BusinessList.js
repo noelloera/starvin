5fc7217af53ecefc3c85bf27d5ebf54870f54334
noelloera <loera.noel@gmail.com>
2020-08-04 19:15:03 -0700

    Added Business List component that renders each business

---
+++ src/Components/BusinessList/BusinessList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Business from "../Business/Business.js";
+import './BusinessList.css'
+
+//The function within the div will iterate through each instance and return an element
+export default class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {this.props.businesses.map(business => {
+          return <Business key={business.id} business={business} />;
+        })}
+      </div>
+    );
+  }
+}
